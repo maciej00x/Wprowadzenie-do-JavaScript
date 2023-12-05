d1847f2906be24af256d0d215b5b573339c9924b
unknown <m.zawadzki022@gmail.com>
2023-12-05 19:28:25 +0100

    zadanie

---
+++ index.js
@@ -0,0 +1,22 @@
+var ageUser = 12;
+const birthdayUser = "12 Marzec";
+
+let isUserHaveBirthday = false;
+
+let age = undefined;
+
+let x = null;
+
+console.log(
+  " Nasz użytkownik ma lat " + ageUser + " urodził się on " + birthdayUser
+);
+
+let array = [1, 2, 3, 4, 5];
+
+let book = {
+  ibn: 1234,
+  autor: " S. King ",
+  listOfTaken: [1, 2, 3, 4, 5],
+};
+
+console.log(book.listOfTaken[-1]);
